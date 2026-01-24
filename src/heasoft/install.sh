7a8dc3abd23776af87fb5de4a06e0452b0497b0b
Aoi <82735346+ryan-minato@users.noreply.github.com>
2026-01-24 14:15:28 +0900

    Add symbolic link workaround for libtclreadline
    Added a workaround to create a symbolic link for libtclreadline-2.1.0.so if it doesn't exist, to support HEASoft installation.

--- src/heasoft/install.sh
+++ src/heasoft/install.sh
@@ -176,7 +176,14 @@ install_apk_deps() {
         tcl-readline
     )
     apk add --no-cache "${pkgs[@]}"
-    ln -sf /usr/lib/tclreadline2.1.0/libtclreadline-2.1.0.so /usr/lib/libtclreadline-2.1.0.so
+    # HACK: Linker fix: Alpine APK lacks libtclreadline.so.2.1.0. 
+    #     Creating a symbolic link from the system-provided version to trick the HEASoft 'make' process.
+    if [ ! -e '/usr/lib/libtclreadline-2.1.0.so' ]; then
+        local source
+        source=$(ls /usr/lib/libtclreadline-*.so 2>/dev/null | sort -V | tail -n 1)
+        echo "Target missing. Creating symlink: ${source} -> /usr/lib/libtclreadline-2.1.0.so"
+        ln -s "${source}" /usr/lib/libtclreadline-2.1.0.so
+    fi
     pip3 install --break-system-packages astropy
 }
 
