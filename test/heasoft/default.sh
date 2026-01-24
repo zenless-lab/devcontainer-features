4db8029a27e856ecd809ea833c3f724e3473d8d3
Aoi <82735346+ryan-minato@users.noreply.github.com>
2026-01-24 17:19:37 +0900

    Rename check for fhelp command to xspec command

--- test/heasoft/default.sh
+++ test/heasoft/default.sh
@@ -6,6 +6,6 @@ source dev-container-features-test-lib
 check "heasoft profile script exists" ls /etc/profile.d/heasoft.sh
 
 check "HEADAS variable is set" echo $HEADAS
-check "fhelp command available" command -v xspec
+check "xspec command available" command -v xspec
 
 reportResults
