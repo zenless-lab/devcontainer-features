bdde8acd7303e79387cb84cabfb9a3eeaef7e7de
Aoi <82735346+ryan-minato@users.noreply.github.com>
2026-03-27 18:13:43 +0900

    fix: tests

--- test/tauri/test.sh
+++ test/tauri/test.sh
@@ -4,18 +4,9 @@ set -e
 
 source dev-container-features-test-lib
 
-# Install Node.js (via NVM), pnpm, and rustup
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-export PATH="$HOME/.local/share/pnpm:$PATH"
-nvm install --lts
-nvm use --lts
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="$HOME/.cargo/bin:$PATH"
+check "node available" node --version
+check "pnpm available" pnpm --version
+check "cargo available" cargo --version
 
 echo "Creating Tauri App..."
 pnpm create tauri-app test-tauri-app --template vanilla --manager pnpm --yes
--- test/tauri/with_rust_and_node_feature.sh
+++ test/tauri/with_rust_and_node_feature.sh
@@ -4,6 +4,10 @@ set -e
 
 source dev-container-features-test-lib
 
+check "node available" node --version
+check "pnpm available" pnpm --version
+check "cargo available" cargo --version
+
 echo "Creating Tauri App..."
 pnpm create tauri-app test-tauri-app --template vanilla --manager pnpm --yes
 
