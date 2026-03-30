9350ec204fe98cfd8beda2da990824e0aecc2269
Aoi <82735346+ryan-minato@users.noreply.github.com>
2026-03-30 11:35:48 +0900

    fix: dev mode

--- test/chezmoi/dev_mode.sh
+++ test/chezmoi/dev_mode.sh
@@ -5,8 +5,8 @@ set -e
 source dev-container-features-test-lib
 
 check "chezmoi version" chezmoi --version
-check "dev mode profile script exists" test -f /etc/profile.d/chezmoi-dev-mode.sh
-check "dev mode alias configured" bash -lc "alias chezmoi | grep -F \"alias chezmoi='chezmoi --source .'\""
-check "dev mode writes source files to current directory" bash -lc 'src_dir=$(mktemp -d); home_dir=$(mktemp -d); export HOME="$home_dir"; touch "$HOME/.bashrc"; cd "$src_dir"; shopt -s expand_aliases; . /etc/profile.d/chezmoi-dev-mode.sh; eval "chezmoi add \"$HOME/.bashrc\""; test -f "$src_dir/empty_dot_bashrc"'
+check "dev mode config exists for remote user" test -f /home/vscode/.config/chezmoi/chezmoi.yaml
+check "source files not in default location" test ! -f /home/vscode/.local/share/chezmoi/dot_bashrc
+check "dev mode writes source files to current directory" chezmoi add /home/vscode/.bashrc && test -f "$PWD/dot_bashrc"
 
 reportResults
--- test/chezmoi/test.sh
+++ test/chezmoi/test.sh
@@ -7,6 +7,9 @@ source dev-container-features-test-lib
 check "chezmoi version" chezmoi --version
 check "chezmoi path" bash -lc '[ "$(readlink -f "$(command -v chezmoi)")" = "/usr/local/bin/chezmoi" ]'
 check "chezmoi ownership and mode" bash -lc '[ "$(stat -c "%U:%G %a" /usr/local/bin/chezmoi)" = "root:root 755" ]'
-check "dev mode disabled by default" bash -lc '[ ! -f /etc/profile.d/chezmoi-dev-mode.sh ]'
+
+check "add bashrc to chezmoi" chezmoi add ~/.bashrc
+check "dot_bashrc existing in default location" test -f ~/.local/share/chezmoi/dot_bashrc
+check "dot_bashrc not existing in current directory" test ! -f "$PWD/dot_bashrc"
 
 reportResults
