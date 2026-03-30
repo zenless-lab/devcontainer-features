7776ba63e17925cf66863986d9614dd027d818b4
Aoi <82735346+ryan-minato@users.noreply.github.com>
2026-03-30 11:39:56 +0900

    fix: ci

--- test/chezmoi/test.sh
+++ test/chezmoi/test.sh
@@ -8,6 +8,8 @@ check "chezmoi version" chezmoi --version
 check "chezmoi path" bash -lc '[ "$(readlink -f "$(command -v chezmoi)")" = "/usr/local/bin/chezmoi" ]'
 check "chezmoi ownership and mode" bash -lc '[ "$(stat -c "%U:%G %a" /usr/local/bin/chezmoi)" = "root:root 755" ]'
 
+echo "# " >> ~/.bashrc
+
 check "add bashrc to chezmoi" chezmoi add ~/.bashrc
 check "dot_bashrc existing in default location" test -f ~/.local/share/chezmoi/dot_bashrc
 check "dot_bashrc not existing in current directory" test ! -f "$PWD/dot_bashrc"
