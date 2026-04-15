7e472a090d8923b61e5083bf5f9e5e46de575994
ryan-minato <82735346+ryan-minato@users.noreply.github.com>
2026-04-15 05:11:06 +0000

    fix: reorganize pre-commit checks and remove redundant test script

--- test/pre-commit/default.sh
+++ test/pre-commit/default.sh
@@ -4,6 +4,8 @@ set -e
 
 source dev-container-features-test-lib
 
-check "workspace git hook exists" bash -lc '[ -f .git/hooks/pre-commit ]'"
+check "pre-commit command available" bash -lc 'command -v pre-commit'
+check "uv tool list includes pre-commit" bash -lc 'uv tool list | grep -Eq "^pre-commit\\b"'
+check "workspace git hook exists" bash -lc '[ -f .git/hooks/pre-commit ]'
 
 reportResults
